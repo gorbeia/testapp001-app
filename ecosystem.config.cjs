cf7940f2e0dd899984f28b090cb45ca980d9028c
gorbeia <inakiibarrola@gmail.com>
2025-12-14 01:06:24 +0100

    pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,11 +6,11 @@ module.exports = {
     exec_mode: 'cluster',
     env: {
       NODE_ENV: 'development',
-      PORT: 3000
+      PORT: 5000
     },
     env_production: {
       NODE_ENV: 'production',
-      PORT: 3000
+      PORT: 5000
     },
     error_file: './logs/err.log',
     out_file: './logs/out.log',
